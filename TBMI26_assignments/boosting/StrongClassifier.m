b3e7bcb0a1c8bec0dcc35190836104a5ade80e9f
Emil Segerbäck <emilsegerback@gmail.com>
2018-02-09 14:55:13 +0100

    Maybe this will conflict

--- TBMI26_assignments/boosting/StrongClassifier.m
+++ TBMI26_assignments/boosting/StrongClassifier.m
@@ -2,6 +2,7 @@ function [H, e] = StrongClassifier(X, Alpha, T, P, K)
 
 Ctot = 0;
 for i = 1:length(T)
+    % Maybe change this because we should use all classifiers on one image and not one classifier on all images
     C = WeakClassifier(T(i), P(i), X(K(i), :));
     Ctot = Ctot + Alpha(i)*C;
 end
